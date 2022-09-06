a01802b4f71ed838ea27de6f3c1e8af7b5c97c6e
Akira Hayashi <smxshxishxad@me.com>
2022-09-06 09:39:14 +0900

    :sparkles: Let `BarkRecognizer` open dog door if bark is detected

--- models/bark_recognizer/bark_recognizer.go
+++ models/bark_recognizer/bark_recognizer.go
@@ -1,11 +1,16 @@
 package bark_recognizer
 
-import "github.com/Rindrics/dogs-door/models/dogdoor"
+import (
+	"fmt"
+
+	"github.com/Rindrics/dogs-door/models/dogdoor"
+)
 
 type BarkRecognizer struct {
 	door dogdoor.DogDoor
 }
 
 func (br *BarkRecognizer) Recognize(bark string) {
-	// recognize given bark
+	fmt.Println("BarkRecognizer: detect ->'", bark, "'")
+	br.door.Open()
 }
