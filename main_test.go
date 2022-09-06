d07cc0e15fd527687cf583a814f16c07545840f3
Akira Hayashi <smxshxishxad@me.com>
2022-09-07 07:04:29 +0900

    :boom: Use goroutine with channel in simulator to make program concurrent

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/Rindrics/dogs-door/models/bark_recognizer"
 	"github.com/Rindrics/dogs-door/models/dogdoor"
@@ -12,20 +13,19 @@ func TestMain(t *testing.T) {
 	door := &dogdoor.DogDoor{}
 	recognizer := bark_recognizer.BarkRecognizer{Door: door}
 
+	done := make(chan bool)
 	fmt.Println("Fido starts to bark:")
 	newBark := "woof"
-	recognizer.Recognize(newBark)
-
+	recognizer.Recognize(newBark, done)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Fido went out")
-
-	if door.IsOpen() {
-		t.Error("door still opened")
-	}
+	<-done
 
 	fmt.Println("Fido finished doing something")
 
 	fmt.Println("Fido starts to bark:")
-	recognizer.Recognize(newBark)
-
+	recognizer.Recognize(newBark, done)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Fido got back to the inside")
+	<-done
 }
